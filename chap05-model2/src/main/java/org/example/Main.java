6a2f123173e746618c2c14532f41ab85f5728bb1
YujeongLEE <dldbwjd24@gmail.com>
2024-05-12 00:33:06 +0900

    chap5 - Entity(Many to Many) : update/delete realtion

--- chap05-model2/src/main/java/org/example/Main.java
+++ chap05-model2/src/main/java/org/example/Main.java
@@ -21,7 +21,10 @@ public class Main {
 
 
             tx.begin(); //트랜잭션 시작
-            testSave(em);  //비즈니스 로직
+            //비즈니스 로직
+            testSave(em);
+            testFind(em);
+
             tx.commit();//트랜잭션 커밋
 
         } catch (Exception e) {
@@ -38,23 +41,56 @@ public class Main {
     public static void testSave(EntityManager em){
 
         //create team1
-        Team team2 = new Team("team2", "팀2");
-        em.persist(team2);
+        Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
 
 
         //create member1
         Member member1 = new Member("member1", "회원1");
-        member1.setTeam(team2);//reference team
+        member1.setTeam(team1);//reference team -> automatically set fk to the table
         em.persist(member1);
 
         //create member2
         Member member2 = new Member("member2", "회원2");
-        member2.setTeam(team2);//reference  team
+        member2.setTeam(team1);//reference  team
         em.persist(member2);
 
 
     }
 
 
+    public static void testFind(EntityManager em){//조회가 영어로 뭐지..
+
+        Member member = em.find(Member.class, "member1");
+        Team team = member.getTeam(); //객체 참조!
+
+        System.out.println("팀 이름 = "+ team.getName());
+    }
+
+
+    public static void updateRelation(EntityManager em){
+
+        Team team2 = new Team("team2","팀2");
+        em.persist(team2);// jpa 기능을 사용하기 위해서는 persist가 꼭 선행되어야 함
+
+        Member member = em.find(Member.class, "member1");
+        member.setTeam(team2); //단순히 set만 진행해도 영속 컨텍스트가 변경을 감지하여 db를 업데이트한다 (em.update 같은 메서드는없음..)
+
+    }
+
+
+    public static void deleteRelation(EntityManager em){
+        Member member1 = em.find(Member.class, "member1");
+        member1.setTeam(null);//연관관계 제거 ( 단순히 set으로 해결)
+
+        /*
+        db와 마찬가지로 연관 엔티티를 삭게하려면, 연관관계 삭제가 선행되어야 한다.
+        ex) team1 제거시 team1에 소속된 모든 member와의 연관관계를 먼저 제거해야함
+        member1.setTeam(null)
+        member2.setTeam(null)
+        em.remove(team1)
+         */
+
+    }
 
 }
